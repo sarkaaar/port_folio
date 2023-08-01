0e58a16628c550c4cb42d095ed4ac9641d852c76
Abdul Wali <waliabdul144@gmail.com>
2023-08-01 22:58:50 +0500

    experience

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -19,23 +19,23 @@ export default function Education() {
     return (
         <section id="education" className="flex justify-center pt-16">
             <div className=" max-w-7xl w-10/12 sm:flex sm:gap-8">
-                <h2 className="py-4 font-bold text-3xl text-gray-100 w-1/5">Education</h2>
+                <h2 className="py-4 font-bold text-3xl text-gray-100 sm:w-64">Education</h2>
                 <hr className=" h-full border-l border-slate-400 hidden sm:block" />
-
-                    <div className="w-full">
+                <div className="w-full">
+                    <div className="flex flex-col gap-8">
                         {experience.map((exp, index) => {
                             return (
-                            <div key={index} className="hover:bg-slate-800 border-r border-slate-500 border-t p-4 my-8 rounded-lg">
-                                <h2 className="text-2xl font-bold">{exp.comp}</h2>
-                                <div className="sm:flex">
-                                    <h3 className="text-lg font-bold text-gray-300 pl-4">{exp.desg}</h3>
-                                    <h3 className="text-md mt-1 text-gray-300 pl-4">{exp.time}</h3>
-                                </div>
-                            </div>)
+                                <div key={index} className="hover:bg-slate-800 border-r border-slate-500 border-t p-4 rounded-lg">
+                                    <h2 className="text-2xl font-bold">{exp.comp}</h2>
+                                    <div className="sm:flex">
+                                        <h3 className="text-lg font-bold text-gray-300 pl-4">{exp.desg}</h3>
+                                        <h3 className="text-md mt-1 text-gray-300 pl-4">{exp.time}</h3>
+                                    </div>
+                                </div>)
                         })}
 
                     </div>
-
+                </div>
             </div>
         </section>
     )
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,20 +10,20 @@ export default function Experience() {
       comp: "Nexusberry",
       desg: "Internee - WEB ",
       time: "(July 2021 - Sept-2021)",
-      disc: "Have Experience in a service based company where I worked as a full stack developep from scratc from node.js to React.js as front end.",
+      disc: "Have Experience in a servi as front end.",
     },
   ];
 
   return (
     <section id="experience" className="flex justify-center pt-16">
       <div className=" max-w-7xl w-10/12 sm:flex sm:gap-8" >
-        <h2 className="py-4 font-bold text-3xl text-gray-100 w-1/5">Experience</h2>
+        <h2 className="py-4 font-bold text-3xl text-gray-100 sm:w-64">Experience</h2>
         <hr className=" h-full border-l border-slate-400 hidden sm:block" />
-        <div className="">
+        <div className="w-full">
           <div className="flex flex-col gap-8">
             {experience.map((exp, index) => {
               return (
-                <div key={index} className="hover:bg-slate-800 border-r border-slate-500 border-t p-4 rounded-lg">
+                <div key={index} className="hover:bg-slate-800 border-r border-slate-500 border-t p-4 rounded-lg sm:w-full">
                   <h2 className="text-2xl font-bold">{exp.comp}</h2>
                   <div className="sm:flex">
                     <h3 className="text-lg font-bold text-gray-300 pl-4">
