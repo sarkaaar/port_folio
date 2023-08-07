2c8afe0567a4999e346a8250717c0bbe50a7f495
awali <awali@i2cinc.com>
2023-08-07 09:36:40 +0500

    footer date and navigations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+
 const navigation = {
 
   social: [
@@ -54,6 +56,9 @@ const navigation = {
 }
 
 export default function Footer() {
+  // const [date, setDate]
+  
+  // useEffect
   return (
     <footer className="bg-slate-900 ">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
@@ -62,14 +67,14 @@ export default function Footer() {
         </nav>
         <div className="mt-10 flex justify-center space-x-10">
           {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a key={item.name} href={item.href} target="_blan" className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
           ))}
         </div>
         <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; 2020 Your Company, Inc. All rights reserved.
+          &copy; {new Date().getFullYear()} AWB, Inc. All rights reserved.
         </p>
       </div>
     </footer>
